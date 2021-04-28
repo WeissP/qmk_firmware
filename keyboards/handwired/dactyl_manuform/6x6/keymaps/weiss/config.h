d41856f60c675d44cfab912a5dc2dcd19317fb06
weiss <kingbaiing@163.com>
2021-04-28 13:51:49 +0200

    init

---
+++ keyboards/handwired/dactyl_manuform/6x6/keymaps/weiss/config.h
@@ -0,0 +1,45 @@
+
+/*
+  Copyright 2012 Jun Wako <wakojun@gmail.com>
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#pragma once
+
+
+#define USE_SERIAL
+
+#define MASTER_LEFT
+
+// #define MASTER_RIGHT
+//#define EE_HANDS
+// Rows are doubled-up
+
+#undef DEBOUNCE
+#define DEBOUNCE 7
+
+#undef TAPPING_TERM
+#define TAPPING_TERM 190
+
+#define _BASIC_LAYER 0
+#define _CUSTOM_LAYER 1
+#define _NUMBER_LAYER 2
+#define _SHIFTED_LAYER 3
+#define _SHIFTED_NUMBER_LAYER 4
+
+#define COMBO_COUNT 1
+
+
